a4c941d828a87a0a90dd70b0bd307319d7b6a5a6
Owen Hartin <owen.hartin@tocomail.com>
2015-12-03 20:27:09 -0600

    Playing with pokeapi.co

--- lib/pokeapi_client.rb
+++ lib/pokeapi_client.rb
@@ -1,3 +1,20 @@
 require "rest-client"
+require "json"
 
-puts RestClient.get('http://pokeapi.co/api/v1/sprite/1/')
+data = RestClient.get 'http://pokeapi.co/api/v1/pokedex/1/'
+read = JSON.parse(data)
+puts JSON.pretty_generate(read)
+
+data = RestClient.get 'http://pokeapi.co/api/v1/pokemon/92/'
+read = JSON.parse(data)
+puts JSON.pretty_generate(read)
+
+data = RestClient.get 'http://pokeapi.co/api/v1/sprite/93/'
+read = JSON.parse(data)
+puts JSON.pretty_generate(read)
+
+data = RestClient.get 'http://pokeapi.co/media/img/92.png'
+File.open('pic.png', 'w') do |file|
+  file.puts(data)
+end
+`open pic.png`
